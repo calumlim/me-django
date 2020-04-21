b066f9703129649a0253f3267d3dcb20ccd2d6f0
Calum <calumstah@gmail.com>
2020-04-21 18:16:54 +0800

    Added Portfolio and Testimony display from database to homepage

--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .models import Portfolio, ClientTestimony
+
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "homepage.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['portfolio'] = Portfolio.objects.all()
+        context['testimonies'] = ClientTestimony.objects.all()
+        return context
